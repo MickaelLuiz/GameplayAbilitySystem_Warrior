05ff4f831168e91496653e23762cd5e2eab1b1e1
Mickael Luiz <mickael.luiz.machado@gmail.com>
2026-04-24 20:50:28 -0300

    Add class structure and debug helper

---
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/WarriorHeroCharacter.h"
+#include "WarriorDebugHelper.h"
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Debug::Print("Warrior Hero Character BeginPlay");
+}
---
+++ Source/Warrior/Public/Characters/WarriorCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WarriorCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class WARRIOR_API AWarriorCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorCharacterBase();
+
+};
