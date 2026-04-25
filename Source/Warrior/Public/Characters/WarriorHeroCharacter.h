af999c8bd5d6c641862343a626c14d93a9141b06
Mickael Luiz <mickael.luiz.machado@gmail.com>
2026-04-25 06:11:12 -0300

    Binding Input

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -6,6 +6,8 @@
 #include "Characters/WarriorCharacterBase.h"
 #include "WarriorHeroCharacter.generated.h"
 
+struct FInputActionValue;
+class UDataAsset_InputConfig;
 class UCameraComponent;
 class USpringArmComponent;
 /**
@@ -21,16 +23,23 @@ public:
 	
 	virtual void BeginPlay() override;
 	
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
 private:
 #pragma region Components
-	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<USpringArmComponent>	CameraBoom;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UCameraComponent> FollowCamera;
-	
 #pragma endregion 
-
+	
+#pragma region Inputs
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UDataAsset_InputConfig> InputConfigDataAsset;
+	
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+#pragma endregion
 	
 };
